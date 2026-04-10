9c4d1c62c50627b4cd5c2ff12dad9013fb0335e4
KK-Bismark <kbismarkkwaku@gmail.com>
2026-04-10 10:02:47 +0100

    created the urls for product details and also the viewing page for the product details

--- ecommerce/curatorial/urls.py
+++ ecommerce/curatorial/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage, shopping_cart
+from .views import homepage, shopping_cart, product_details
 
 
 urlpatterns = [
         path("", homepage, name="homepage"),
         path("cart/", shopping_cart, name="shopping_cart"),
+        path("product_details/", product_details, name="product_details")
         ]
--- ecommerce/curatorial/views.py
+++ ecommerce/curatorial/views.py
@@ -6,3 +6,6 @@ def homepage(request):
 
 def shopping_cart(request):
     return render(request, "shopping_cart.html")
+
+def product_details(request):
+    return render(request, "product_details.html")
